c58abcf7d985b44594bf9acad12a9cdf128e4f98
Ronsho <ron@ftbpro.com>
2013-11-23 19:43:56 +0200

    changes

--- spec/lib/post_create_spec.rb
+++ spec/lib/post_create_spec.rb
@@ -12,10 +12,11 @@ describe "Post Created" do
     @league_writers = create :LeagueWriters, { id: @league_id }
     @team_counters = create :TeamCounters, { id: @team_id }
     @team_writers = create :TeamWriters, { id: @team_id }
+    @locale = "en"
   end
 
   before :all do
-    open("http://#{HOST}/post_create?user=#{@user.id}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id}&league_count=true&writers_count=true&ulb=false")
+    open("http://#{HOST}/post_create?user=#{@user.id}&post=#{@post.id}&locale=#{@locale}&league=#{@league_id}&team=#{@team_id}&league_count=true&writers_count=true&ulb=false")
   end
 
   it "should increase the user's num of posts created by 1" do
@@ -43,7 +44,7 @@ describe "Post Created" do
       end
 
       it "should not increase the league posts count if league_count is false" do
-        open("http://#{HOST}/post_create?user=#{@user.id}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id + 1}&league_count=0&writers_count=true&ulb=false")
+        open("http://#{HOST}/post_create?user=#{@user.id}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id + 1}&league_count=0&writers_count=true&locale=#{@locale}ulb=false")
         @league_counters.data["posts"].to_i.should eq @league_counters.initial_data["posts"].to_i + 1
       end
     end
@@ -54,17 +55,17 @@ describe "Post Created" do
       end
 
       it "should not increase the number of writers for the league if its not a new writer" do
-        open("http://#{HOST}/post_create?user=#{@user.id.to_i}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id + 1}&league_count=true&writers_count=true&ulb=false")
+        open("http://#{HOST}/post_create?user=#{@user.id.to_i}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id + 1}&league_count=true&writers_count=true&locale=#{@locale}&ulb=false")
         @league_counters.data["writers"].to_i.should eq @current_data["writers"].to_i
       end
 
       it "should not increase the number of writers for the league if its a new writer but writers_count is false" do
-        open("http://#{HOST}/post_create?user=#{@user.id.to_i + 1}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id + 1}&league_count=true&writers_count=false&ulb=false")
+        open("http://#{HOST}/post_create?user=#{@user.id.to_i + 1}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id + 1}&league_count=true&writers_count=false&locale=#{@locale}&ulb=false")
         @league_counters.data["writers"].to_i.should eq @current_data["writers"].to_i
       end
 
       it "should increase the number of writers for the league if its a new writer" do
-        open("http://#{HOST}/post_create?user=#{@user.id.to_i + 1}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id + 1}&league_count=true&writers_count=true&ulb=false")
+        open("http://#{HOST}/post_create?user=#{@user.id.to_i + 1}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id + 1}&league_count=true&writers_count=true&locale=#{@locale}&ulb=false")
         @league_counters.data["writers"].to_i.should eq @current_data["writers"].to_i + 1
       end
     end
@@ -76,7 +77,7 @@ describe "Post Created" do
     end
 
     it "should not add 1 to the user who created the post if writers_count is false" do
-      open("http://#{HOST}/post_create?user=#{@user.id}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id + 1}&league_count=true&writers_count=false&ulb=false")
+      open("http://#{HOST}/post_create?user=#{@user.id}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id + 1}&league_count=true&writers_count=false&locale=#{@locale}&ulb=false")
       @league_writers.set["user_#{@user.id}"].to_i.should == @league_writers.initial_set["user_#{@user.id}"].to_i + 3  # greater by 3 because of previous spec who does another call
     end
   end
@@ -88,7 +89,7 @@ describe "Post Created" do
       end
 
       it "should not increase the team posts count if league_count is false" do
-        open("http://#{HOST}/post_create?user=#{@user.id}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id}&league_count=0&writers_count=true&ulb=false")
+        open("http://#{HOST}/post_create?user=#{@user.id}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id}&league_count=0&writers_count=true&locale=#{@locale}&ulb=false")
         @team_counters.data["posts"].to_i.should eq @team_counters.initial_data["posts"].to_i + 1
       end
     end
@@ -99,33 +100,33 @@ describe "Post Created" do
       end
 
       it "should not increase the number of writers for the team if its not a new writer" do
-        open("http://#{HOST}/post_create?user=#{@user.id.to_i}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id}&league_count=true&writers_count=true&ulb=false")
+        open("http://#{HOST}/post_create?user=#{@user.id.to_i}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id}&league_count=true&writers_count=true&locale=#{@locale}&ulb=false")
         @team_counters.data["writers"].to_i.should eq @current_data["writers"].to_i
       end
 
       it "should not increase the number of writers for the team if its a new writer but writers_count is false" do
-        open("http://#{HOST}/post_create?user=#{@user.id.to_i + 1}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id}&league_count=true&writers_count=false&ulb=false")
+        open("http://#{HOST}/post_create?user=#{@user.id.to_i + 1}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id}&league_count=true&writers_count=false&locale=#{@locale}&ulb=false")
         @team_counters.data["writers"].to_i.should eq @current_data["writers"].to_i
       end
 
       it "should increase the number of writers for the team if its a new writer" do
-        open("http://#{HOST}/post_create?user=#{@user.id.to_i + 1}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id}&league_count=true&writers_count=true&ulb=false")
+        open("http://#{HOST}/post_create?user=#{@user.id.to_i + 1}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id}&league_count=true&writers_count=true&locale=#{@locale}&ulb=false")
         @team_counters.data["writers"].to_i.should eq @current_data["writers"].to_i + 1
       end
 
       it "should increase the number of original writers if ulb parameter is not false and the user is a new writer" do
-        open("http://#{HOST}/post_create?user=#{@user.id.to_i + 2}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id}&league_count=true&writers_count=true&ulb=true")
+        open("http://#{HOST}/post_create?user=#{@user.id.to_i + 2}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id}&league_count=true&writers_count=true&locale=#{@locale}&ulb=true")
         @team_counters.data["original_writers"].to_i.should eq @current_data["original_writers"].to_i + 1
       end
 
       it "should not increase the number of original writers if ulb parameter is false " do
-        open("http://#{HOST}/post_create?user=#{@user.id.to_i + 3}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id}&league_count=true&writers_count=true&ulb=false")
-        @team_counters.data["original_writers"].to_i.should eq @current_data["original_writers"].to_i + 1       
+        open("http://#{HOST}/post_create?user=#{@user.id.to_i + 3}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id}&league_count=true&writers_count=true&locale=#{@locale}&ulb=false")
+        @team_counters.data["original_writers"].to_i.should eq @current_data["original_writers"].to_i + 1
       end
 
       it "should not increase the number of original writers if ulb parameter is true but writer already exist " do
-        open("http://#{HOST}/post_create?user=#{@user.id.to_i + 3}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id}&league_count=true&writers_count=true&ulb=false")
-        @team_counters.data["original_writers"].to_i.should eq @current_data["original_writers"].to_i + 1       
+        open("http://#{HOST}/post_create?user=#{@user.id.to_i + 3}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id}&league_count=true&writers_count=true&locale=#{@locale}&ulb=false")
+        @team_counters.data["original_writers"].to_i.should eq @current_data["original_writers"].to_i + 1
       end
     end
   end
@@ -136,7 +137,7 @@ describe "Post Created" do
     end
 
     it "should not add 1 to the user who created the post if writers_count is false" do
-      open("http://#{HOST}/post_create?user=#{@user.id}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id}&league_count=true&writers_count=false")
+      open("http://#{HOST}/post_create?user=#{@user.id}&post=#{@post.id}&league=#{@league_id}&team=#{@team_id}&league_count=true&writers_count=false&locale=#{@locale}")
       @team_writers.set["user_#{@user.id}"].to_i.should == @team_writers.initial_set["user_#{@user.id}"].to_i + 3  # greater by 3 because of previous spec who does another call
     end
   end
