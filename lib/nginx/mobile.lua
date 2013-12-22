d985c9cc2f68f5a80c302ff1be033f39f6c961ca
Ronsho <ron@ftbpro.com>
2013-12-22 10:56:26 +0200

    dont using keep_alive if there is an error with redis

--- lib/nginx/mobile.lua
+++ lib/nginx/mobile.lua
@@ -9,6 +9,7 @@ local red = utils:initRedis()
 
 local ok, err = red:evalsha(ngx.var.redis_mobile_hash, 1, "args", args_json)
 --Because we have some mobile devices with the old api, don't logging errors cause there are LOTS of from them!
+if not ok then utils:emptyGif() end
 
 ok, err = red:set_keepalive(10000, 100)
 if not ok then ngx.log(ngx.ERR, "Error setting redis keep alive from MOBILE " .. err) end
