23086fff3474912e81cfc3a1c57afb9b731b4038
Shai Kerer <shai@ftbpro.com>
2013-12-31 18:07:06 +0200

    refactored s3 monitor

--- lib/scripts/monitors/s3-monitor.sh
+++ lib/scripts/monitors/s3-monitor.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 BUCKET_NAME=$1
 FOLDER_TO_CHECK=$(date +%Y/%m/%d -d "yesterday")
-RESPONSE=$(aws s3 ls s3://$BUCKET_NAME/$FOLDER_TO_CHECK)
 
-if echo "$RESPONSE" | grep ".gz"; then
-  echo "matched"
-else
-  echo "No S3 backups for $FOLDER_TO_CHECK." | mail -s "Action Counter Monitor" ron@ftbpro.com, dor@ftbpro.com, shai@ftbpro.com
-fi
+logs_backup $FOLDER_TO_CHECK
+logs_backup /access_logs/$FOLDER_TO_CHECK
+
+logs_backup() 
+{
+	YESTERDAY_BK="$(aws s3 ls s3://$BUCKET_NAME/$1/ | grep ".gz" | wc -l)"
+	if [ "$YESTERDAY_BK" -ne "24" ]; then
+		echo "Missing S3 backups for $1." | mail -s "Action Counter Monitor" ron@ftbpro.com, dor@ftbpro.com, shai@ftbpro.com
+	fi				
+}
+
